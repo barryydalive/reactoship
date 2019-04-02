3992650289a9d300edf6dff5e6be6f419f38ac38
Barry Huang <Barry.huang93@gmail.com>
2019-04-02 16:05:36 -0400

    client

--- server/index.js
+++ server/index.js
@@ -9,13 +9,14 @@ app.use(express.static(path.join(__dirname, '../client/build')))
 app.get('/api/getList', (req, res) => {
   const list = [ 'item1', 'item2', 'item3', ]
   res.json(list)
-  console.log('Sent list of items')
 })
 
 // Handles any requests that don't match the ones above
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   // res.send('hi')
-  res.sendFile(path.join(__dirname + '../client/build/index.html'))
+  console.log(path.join(__dirname + '../client/build/index.html'))
+  console.log('hi')
+  // res.sendFile(path.join(__dirname + '../client/build/index.html'))
 })
 
 const port = process.env.PORT || 5000
