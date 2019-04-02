57aa31cd09f57339fb5f92cdbd6219ba28965512
Barry Huang <Barry.huang93@gmail.com>
2019-04-02 15:45:12 -0400

    hotfix

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ app.get('/api/getList', (req, res) => {
 
 // Handles any requests that don't match the ones above
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+  res.send('hi')
+  // res.sendFile(path.join(__dirname + '/../client/build/index.html'))
 })
 
 const port = process.env.PORT || 5000
